90f09fad58fb329fb984203a75d6a980f991957d
eyal360now <79267396+eyal360now@users.noreply.github.com>
2021-12-30 18:37:02 +0200

    Eyal - Change table field sizes and add table relations.

--- src/SqlUtils/createUsersTable.sql
+++ src/SqlUtils/createUsersTable.sql
@@ -1,11 +1,11 @@
 USE yaron_db;
 
 CREATE TABLE IF NOT EXISTS users
-( first_name varchar(250) NOT NULL,
-  last_name varchar(250) NOT NULL,
-  user_name varchar(250) NOT NULL,
-  password varchar(250),
-  email_id varchar(250) NOT NULL,
-  mobile_number varchar(250) NOT NULL,
+( first_name varchar(20) NOT NULL,
+  last_name varchar(30) NOT NULL,
+  user_name varchar(15) NOT NULL,
+  password varchar(15),
+  email_id varchar(50) NOT NULL,
+  mobile_number varchar(15) NOT NULL,
   PRIMARY KEY (user_name)
 );
--- src/SqlUtils/newCreateGamesTables.sql
+++ src/SqlUtils/newCreateGamesTables.sql
@@ -1,14 +1,14 @@
 USE yaron_db;
 
 CREATE TABLE IF NOT EXISTS countries
-( country_id varchar(35) NOT NULL,
-  country_name varchar(70) NOT NULL,
+( country_id varchar(2) NOT NULL,
+  country_name varchar(50) NOT NULL,
   PRIMARY KEY (country_id)
 );
 
 CREATE TABLE IF NOT EXISTS cities
 ( city_name varchar(70) NOT NULL,
-  country_id varchar(35) NOT NULL,
+  country_id varchar(2) NOT NULL,
   PRIMARY KEY (city_name,country_id),
   FOREIGN KEY (country_id) REFERENCES countries(country_id)
 );
@@ -20,7 +20,7 @@ CREATE TABLE IF NOT EXISTS game_details
   game_date DATETIME NOT NULL,
   creation_date DATETIME NOT NULL,
   city varchar(70) NOT NULL,
-  sport_category varchar(20) NOT NULL,
+  sport_category varchar(15) NOT NULL,
   players int NOT NULL,
   level int NOT NULL,
   PRIMARY KEY (user_name, game_name, creation_date),
@@ -29,10 +29,10 @@ CREATE TABLE IF NOT EXISTS game_details
 
 
 CREATE TABLE IF NOT EXISTS match_games
-( user_name varchar(35) NOT NULL,
-  game_name varchar(15) NOT NULL,
+( user_name varchar(15) NOT NULL,
+  game_name varchar(35) NOT NULL,
   creation_date DATETIME NOT NULL,
-  participant varchar(35) NOT NULL,
+  participant varchar(15) NOT NULL,
   PRIMARY KEY (user_name, game_name, creation_date, participant),
   FOREIGN KEY(user_name, game_name, creation_date) REFERENCES game_details(user_name, game_name, creation_date)
 );
@@ -45,7 +45,7 @@ CREATE TABLE IF NOT EXISTS game_details_archives
    game_date DATETIME NOT NULL,
    creation_date DATETIME NOT NULL,
    city varchar(70) NOT NULL,
-   sport_category varchar(20) NOT NULL,
+   sport_category varchar(15) NOT NULL,
    players int NOT NULL,
    level int NOT NULL,
    FOREIGN KEY(city) REFERENCES cities(city_name),
